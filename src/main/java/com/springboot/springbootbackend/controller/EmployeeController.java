71f935ff38305850722962f0c1d16d7247c04da7
lax <codewithlax@gmail.com>
2023-05-23 16:25:04 +0530

    Complete SpringBoot application with Spring Data JPA dependency, MySQL DB & CRUD operation

--- src/main/java/com/springboot/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/springboot/springbootbackend/controller/EmployeeController.java
@@ -2,12 +2,12 @@ package com.springboot.springbootbackend.controller;
 
 import com.springboot.springbootbackend.model.Employee;
 import com.springboot.springbootbackend.service.EmployeeService;
+import com.springboot.springbootbackend.service.impl.EmployeeServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -26,4 +26,34 @@ public class EmployeeController {
         return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
     }
 
+    //Build get all employees Rest API
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    //build get employee by ID REST api
+    //localhost:8080/api/employees/1
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId) {
+        return new ResponseEntity<>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+    }
+
+    //build update employee REST API
+    //localhost:8080/api/employees/1
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id,
+                                                   @RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+    }
+
+    //build delete employee REST API
+    //localhost:8080/api/employees/1
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
+
+        //delete Employee from DB
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>("Employee Delete Successfully!.", HttpStatus.OK);
+    }
 }
--- src/main/java/com/springboot/springbootbackend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/springbootbackend/service/impl/EmployeeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.springboot.springbootbackend.service.impl;
 
+import com.springboot.springbootbackend.exception.ResourceNotFoundException;
 import com.springboot.springbootbackend.model.Employee;
 import com.springboot.springbootbackend.repository.EmployeeRepository;
 import com.springboot.springbootbackend.service.EmployeeService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -18,5 +22,55 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        //Old Approach
+        /*
+        Optional<Employee> employee =  employeeRepository.findById(id);
+
+        if(employee.isPresent()){
+            return employee.get();
+        }
+        else{
+            throw new ResourceNotFoundException("Employee","Id",id);
+        }
+        */
+
+        //Using java 8 feature
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", id));
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee, long id) {
+        //we need to check whether employee with given id is existed in DB or not
+        Employee exsitingEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", id));
+
+        exsitingEmployee.setFirstName(employee.getFirstName());
+        exsitingEmployee.setLastName(employee.getLastName());
+        exsitingEmployee.setEmail(employee.getEmail());
+
+        //Save the existing Employee to DB
+        employeeRepository.save(exsitingEmployee);
+
+        return exsitingEmployee;
+
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+        //Check whether a Employee exist in DB or not
+        employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", id));
+
+        employeeRepository.deleteById(id);
     }
 }
